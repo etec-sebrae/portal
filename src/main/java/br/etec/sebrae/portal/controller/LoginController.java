4168cdcda480e7aae754dd467223d3a90e05a75a
Francisco Aurizelio <francisco.souza63@etec.sp.gov.br>
2020-05-27 23:21:13 -0300

    criação do projeto portal

--- src/main/java/br/etec/sebrae/portal/controller/LoginController.java
+++ src/main/java/br/etec/sebrae/portal/controller/LoginController.java
@@ -1,15 +1,39 @@
 package br.etec.sebrae.portal.controller;
 
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.etec.sebrae.portal.dtos.LoginDto;
 
 @Controller
 public class LoginController {
-
-	@RequestMapping("/logar")
-	public String logar() {
-		return "login/login";
-		
-	}
 	
+	//Logger log = LogFactory.getLog(LoginController.class);	
+	
+	@RequestMapping("/logar")
+	public String logar(LoginDto login) {
+		
+		String emailApi = "admin@admin.com";
+		String senhaApi = "123456";
+		
+		if(emailApi.equals(login.getEmail()) && senhaApi.equals(login.getPassword())) {
+			return "redirect:solicitacoes";	
+		}		
+		return "login/cadastro";		
+	}	
+	
+	@RequestMapping("/solicitacoes")
+	public ModelAndView solicitacoes() {			
+		ModelAndView view = new ModelAndView("documentos/solicitacoes");		
+		return view;		
+	}
+
+	@RequestMapping("/cadastrologin")
+	public ModelAndView cadastrarLogin(LoginDto login) {
+		ModelAndView view = new ModelAndView("index");		
+		view.addObject("sucesso", "Cadastro efetuado com sucesso!");
+		return view	;	
+	}	
 }
